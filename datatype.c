f5041d87f47c71664bb5a3cabe0f63ae53d2db0a
aaronbroderick <aarontbroderick@icloud.com>
2025-10-28 21:04:39 -0500

    Completed #4

--- datatype.c
+++ datatype.c
@@ -8,12 +8,19 @@ int main() {
     double myDouble = 3.14159;
     char myChar = 'A';
     printf("%d\n%f\n%lf\n%c\n", myInt, myFloat, myDouble, myChar);
-*/
+
     // 3
     char a = 'a', b = 'b', c1 = 'c', x = 'x', y = 'y', z = 'z', A = 'A', B = 'B', C = 'C', X = 'X', Y = 'Y', Z = 'Z';
     printf("Using characters: %c%c%c%c%c%c%c%c%c%c%c%c\n", a,b,c1,x,y,z,A,B,C,X,Y,Z);
     char a2 = 97, b2 = 98, c2 = 99, x2 = 120, y2 = 121, z2 = 122, A2 = 65, B2 = 66, C2 = 67, X2 = 88, Y2 = 89, Z2 = 90; 
     printf("Using ASCII: %c%c%c%c%c%c%c%c%c%c%c%c\n", a2,b2,c2,x2,y2,z2,A2,B2,C2,X2,Y2,Z2);
+*/
 
+    // 4
+    float fs = 35e3;
+    double ds = 12e4;
+    printf("Float: %f\n", fs);
+    printf("Double: %lf\n", ds);
+    
     return 0;
 }
