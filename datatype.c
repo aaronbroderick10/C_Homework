0f30e8d0155ec288a5f7d90244ad93e0c380e89f
aaronbroderick <aarontbroderick@icloud.com>
2025-10-28 21:12:17 -0500

    Completed #5

--- datatype.c
+++ datatype.c
@@ -14,13 +14,21 @@ int main() {
     printf("Using characters: %c%c%c%c%c%c%c%c%c%c%c%c\n", a,b,c1,x,y,z,A,B,C,X,Y,Z);
     char a2 = 97, b2 = 98, c2 = 99, x2 = 120, y2 = 121, z2 = 122, A2 = 65, B2 = 66, C2 = 67, X2 = 88, Y2 = 89, Z2 = 90; 
     printf("Using ASCII: %c%c%c%c%c%c%c%c%c%c%c%c\n", a2,b2,c2,x2,y2,z2,A2,B2,C2,X2,Y2,Z2);
-*/
+
 
     // 4
     float fs = 35e3;
     double ds = 12e4;
     printf("Float: %f\n", fs);
     printf("Double: %lf\n", ds);
+    */
+    
+    //5
+    float floatVar = 3.5;
+    printf("Precision 1: %.1f\n", floatVar);
+    printf("Precision 2: %.2f\n", floatVar);
+    printf("Precision 4: %.4f\n", floatVar);
+    printf("Precision 6: %f\n", floatVar);
     
     return 0;
 }
