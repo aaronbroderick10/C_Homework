d14b1a9c5c04137a087dcea53cb8bba424494005
aaronbroderick <aarontbroderick@icloud.com>
2025-10-28 20:37:05 -0500

    Completed #2

---
+++ datatype.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int myInt = 67;
+    float myFloat = 3.14;
+    double myDouble = 3.14159;
+    char myChar = 'A';
+    printf("%d\n%f\n%lf\n%c\n", myInt, myFloat, myDouble, myChar);
+    return 0;
+}
