4413315a55a81e2607bd1f0878b2a803cafbc024
aaronbroderick <aarontbroderick@icloud.com>
2025-10-28 21:55:53 -0500

    Completed #2

---
+++ arithmetic.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    
+    //1
+    int x = 0;
+    printf("x: %d\n", x);   
+    printf("x++: %d\n", x++);   
+    x = 0;
+    printf("x: %d\n", x);   
+    printf("++x: %d\n", ++x);   
+    x = 0;
+    printf("x: %d\n", x);   
+    printf("x--: %d\n", x--);   
+    x = 0;
+    printf("x: %d\n", x);   
+    printf("--x: %d\n", --x);   
+
+    return 0;
+}
